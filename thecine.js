4116fa67142c7e5ccd23e45eadc8d7ef2c4be87a
caiomrb <105893692+caiomrb@users.noreply.github.com>
2022-05-25 20:22:22 -0300

    Initial commit

---
+++ thecine.js
@@ -0,0 +1,46 @@
+function entrar() {
+    var poster = window.document.getElementById('interacao')
+    poster.src = 'imagens/sinopsetakecare.jpg'
+}
+
+function clicar() {
+    var poster = window.document.getElementById('interacao')
+    poster.src = 'imagens/sessoesTHEcinesala1.png'
+}
+
+function sair() {
+    var poster = window.document.getElementById ('interacao')
+    poster.src = 'imagens/pexels-merve-bayarcorrecao.jpg'
+}
+
+function comein() {
+    var filme = window.document.getElementById('detalhes')
+    filme.src = 'imagens/sinopsesMYmineMY.jpg'
+}
+
+function selecionar() {
+    var filme = window.document.getElementById('detalhes')
+    filme.src = 'imagens/sessoesTHEcinesala2.png'
+}
+
+function leave() {
+    var filme = window.document.getElementById('detalhes')
+    filme.src = 'imagens/myminemynovo.jpg'
+
+}
+
+function getin() {
+    var cinema = window.document.getElementById('informacoes')
+    cinema.src = 'imagens/sinopseTilliTHppns.jpg'
+}
+
+ function apertar() {
+     var cinema = window.document.getElementById('informacoes')
+     cinema.src = 'imagens/sessoesTHEcinesala3.png'
+ }
+
+ function comeout() {
+     var cinema = window.document.getElementById('informacoes')
+     cinema.src = 'imagens/tillithappenstoyou - Copia - Copia.jpg'
+ }
+
